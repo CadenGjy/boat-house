7e0595077c952437941f429e1081709176e74c05
cadenguo <cadenguo@leansoftx.com>
2020-02-09 22:21:58 +0800

    55

--- pipelines/boathouse-management.groovy
+++ pipelines/boathouse-management.groovy
@@ -9,8 +9,7 @@ pipeline {
             steps {
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']],
                             userRemoteConfigs: [[url: 'https://github.com/CadenGjy/boat-house.git']]])                                     
-                sh 'cd ./management/web
-                    docker build -f Dockerfile -t tool.devopshub.cn:2020/idcps/management:test .'
+                sh 'cd ./management/web;docker build -f Dockerfile -t tool.devopshub.cn:2020/idcps/management:test .'
             }
         }
         stage('Dev') { 
